18f485560c712363c2e7882ec90d2ff407e325de
RuppObsMicA <shalkovskyrg@gmail.com>
2023-03-21 01:06:07 +0300

    Added choosing of star rating

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const tabsNavs = document.querySelectorAll(".tabs-nav");
+const starsContainer = document.querySelectorAll(".star");
 
 tabsNavs.forEach((tabNav, idx) => {
     tabNav.addEventListener('click', e => {
@@ -11,4 +12,38 @@ tabsNavs.forEach((tabNav, idx) => {
         document.getElementById("tab_" + idx).style.display = "block";
     })
 })
+
+
+
+for (let i = 0; i < starsContainer.length; i++) { //add events to all stars
+    starsContainer[i].addEventListener("mouseover", hoverStarRating);
+    starsContainer[i].addEventListener("mouseout", removeStarRating);
+    starsContainer[i].addEventListener("click", saveData);
+}
+
+function hoverStarRating(){ //add color to all stars before a hovered one
+    for (let i = 1; i <= starsContainer.length; i++){
+        if (this.classList.contains("star_" + i)){  //find what star was hovered and set color for all previous ones
+            for (let j = 0; j < i; j++){
+                starsContainer[j].style.color = "gold";
+            }
+        }
+    }
+}
+function saveData (){ //save the rating if we need
+    let numberOfStar = this.classList[this.classList.length-1];
+    alert(numberOfStar.slice(numberOfStar.length-1,numberOfStar.length));
+}
+
+function removeStarRating(){ //return default color
+    for (let i = 0; i < starsContainer.length; i++){ //just to change ratings without refreshing the page, can be removed
+        starsContainer[i].style.color = "gray";
+    }
+}
+
+
+
+
+
+
 
